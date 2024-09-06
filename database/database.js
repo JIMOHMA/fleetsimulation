1162f0186c0a05a612afad874f892b6c03af03f3
JIMOHMA <mail4ayodele@gmail.com>
2024-09-06 07:58:26 -0400

    new acqusition process done... creates company, it's vehicles, its maintenance history and automatic simulation of sensory data

--- database/database.js
+++ database/database.js
@@ -12,30 +12,44 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const connectionString = process.env.connString
 const client = new MongoClient(connectionString);
 
-// uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
-async function loadCompany() {
-    const companyA = new company("CompanyA", 5, "Hamilton Ontario, Canada");
-    const companyB = new company();
+// new acquisition
+// Steps:  1. create company in db
+//         2. create vehicles associated with company in db
+//         3. Initialize vehicles historical data
+//         4. Implicit operation: simulataion.js automatically would query this new
+//         acquisition and simulate the sensory data accordingly
+async function newAcquisition() {
+
+    const companyId = uuidv4();
+    // a new company has between 1 and 7
+    const vehicleNum = Math.ceil(Math.random()*7); 
     const newCompany = new Company({
-        companyId: uuidv4(),
-        name: companyB.companyName,
-        numberOfVehicles: companyB.numOfVehicle,
-        location: companyB.location,
-        vehicles: []
+        _id: companyId,
+        companyId: companyId,
+        name: generateRandomCompanyName(),
+        numberOfVehicles: vehicleNum,
+        location: "Mississauga Ontario, Canada",
+        vehicles: await createVehicleInformation(vehicleNum, companyId)
     })
 
     try {
+        await client.connect();
+        const database  = client.db('FleetElement');
+        const companyCollection = database.collection('companies');
+
         const result = await companyCollection.insertOne(newCompany)
         console.log(result)
+        console.log(`Successfully added new client`)
     } catch (error) {
-        console.log("Error", error)
+        console.log("Error inserting company", error)
     } finally {
         await client.close();
     }
-}
- 
-// loadCompany()
 
+    initializeFleetHistory(companyId)
+}
+
+// uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 const vehicleTypes = [
     "Pickup Truck",
     "Box Truck",
@@ -45,6 +59,62 @@ const vehicleTypes = [
     "Tank Truck",
     "Semi Truck"
 ];
+
+
+const initializeFleetHistory = async (companyId) => {
+    await client.connect();
+    const db = client.db("FleetElement")
+    const companyCollection = db.collection('companies')
+
+    const query = { companyId: companyId};
+    const projection = {companyId: 1, vehicles: 1} 
+    const result = await companyCollection.findOne(query, {projection: projection});
+
+    // using the result, simulate maintenance history a vehicle
+    let maintenanceDataInfo = []
+    result.vehicles.forEach((vehicleId) => {
+        let lastOilChange = {date: new Date().toISOString(), longitude: "78.77", latitude: "75.28"}
+        let nextOilChangeDate = new Date(lastOilChange.date)
+        nextOilChangeDate.setMonth(nextOilChangeDate.getMonth() + 2) // next oil change in 2 months
+        
+        const maintenanceDataID = uuidv4()
+        const maintenanceData = new MaintenanceInformation({
+            _id: maintenanceDataID,
+            infoId: maintenanceDataID,
+            vehicleId: vehicleId,
+            companyId: companyId,
+            lastOilChange: lastOilChange,
+            nextOilChangeDate: nextOilChangeDate,
+            tirePressureRefill: {
+                date: new Date().toISOString(), 
+                longitude: "78.77",
+                latitude: "75.28",
+                oldValue: [pValue(30), pValue(33), pValue(31), pValue(34)], // [fr, fl, rr, rl]
+                refillValue: [newPValue(50), newPValue(50), newPValue(50), newPValue(50)], // [fr, fl, rr, rl]                    
+            },
+            refuelHistory: {
+                date: new Date().toISOString(), 
+                longitude: "-79.63594368066431",
+                latitude: "43.59740068079627",
+                oldValue: 0.25, // 0 | 0.25 | 0.5 | 1.75 | 1
+                refuelLevel: 1
+            }
+        })
+        maintenanceDataInfo.push(maintenanceData)
+    })
+
+    // insert all the sensory data into the database
+    try {
+        const maintenanceCollection = db.collection('maintenanceData');
+        const insertionResult = await maintenanceCollection.insertMany(maintenanceDataInfo)
+        console.log(insertionResult)
+        console.log("Insertion of data from maintenance successful")
+    } catch (error) {
+        console.log("Maintenance data insertion error is:", error)
+    } finally {
+        await client.close()
+    }
+}
 
 // should return a list of the vehicleIds created
 async function createVehicleInformation(vehicleNum, companyId) {
@@ -86,37 +156,6 @@ async function createVehicleInformation(vehicleNum, companyId) {
 
     return vehicleIdList // list of vehicles created for a company
 }
-
-async function newClient() {
-
-    const newID = uuidv4();
-    // a new company has between 1 and 7
-    const vehicleNum = Math.ceil(Math.random()*7); 
-    const newCompany = new Company({
-        _id: newID,
-        companyId: newID,
-        name: generateRandomCompanyName(),
-        numberOfVehicles: vehicleNum,
-        location: "Mississauga Ontario, Canada",
-        vehicles: await createVehicleInformation(vehicleNum, newID)
-    })
-
-    try {
-        await client.connect();
-        const database  = client.db('FleetElement');
-        const companyCollection = database.collection('companies');
-
-        const result = await companyCollection.insertOne(newCompany)
-        console.log(result)
-        console.log(`Successfully added new client`)
-    } catch (error) {
-        console.log("Error inserting company", error)
-    } finally {
-        await client.close();
-    }
-}
-
-// newClient();
 
 const pValue = (bound) => {
     return Math.floor(Math.random()*15) + bound
@@ -185,4 +224,7 @@ const maintenanceHistory = async () => {
     }
 }
 
-maintenanceHistory()
+// add a new company
+newAcquisition()
+
+
