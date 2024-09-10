bfab5875d1e6be1f9d3311b99e226529ad3bc334
JIMOHMA <mail4ayodele@gmail.com>
2024-09-10 18:46:09 -0400

    Made the header tag from index.html into it's own component which fixed the weird reload behaviour when navbar is clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
+import { Link } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link';
+
 function Header() {
   return (
-    <div>
-      <h1>This is the Header</h1>
-    </div>
+    <>
+      <header>
+        <h3>
+          <Link className="text-on-light" to={'/home'}>Fleet Manager</Link>
+        </h3>
+        <nav>
+            <Link to={"/home"}>Home</Link>
+            <Link to={"/acquisitions"}>Acquisitions</Link>
+            <HashLink to="/#aboutelement">About</HashLink>
+        </nav>
+      </header>
+    </>
   )
 }
 
