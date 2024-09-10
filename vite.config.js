70a97686dda26da4142fd0cb458584921927facb
JIMOHMA <mail4ayodele@gmail.com>
2024-09-10 03:45:40 -0400

    Update vite config file with port number and host IP for deployment on Render

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4000, // Choose a port that Render expects (e.g., 3000, 5000)
+    host: '0.0.0.0', // Ensure the server listens on all network interfaces
+  },
 })
