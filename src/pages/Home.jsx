3a82a173ae4d51bfba3c4c16081b4254dfcc02f3
JIMOHMA <mail4ayodele@gmail.com>
2024-09-10 23:35:11 -0400

    Re-designed home page with image covering the background of the hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,17 +24,19 @@ function Home() {
     <>
       <Header />
       <div className='hero-section'>
-        <h6>Welcome to</h6>
-        <h1 className='hero-header'>Fleet Simulation</h1>
-        <p className="hero-description">
-          Access real-time analytics, 
-          and explore how your fleet of vehicles can be better managed.
-        </p>
-        {/* <a className="hero-button" href="/new">New Acquisition</a> */}
-        <Link className='hero-button' to={'/new'}>New Acquisition</Link>
-
+        <div className="hero-content">
+          <h6>Welcome to</h6>
+          <h1 className='hero-header'>Fleet Simulation</h1>
+          <p className="hero-description">
+            Access real-time analytics, 
+            and explore how your fleet of vehicles can be better managed.
+          </p>
+          
+          {/* <a className="hero-button" href="/new">New Acquisition</a> */}
+          <Link className='hero-button' to={'/new'}>New Acquisition</Link>
+        </div>
         <figure className='hero-bg'>
-          <img src="/images/fleetVehicles.jfif" alt="Fleet of vehicles as background" />
+          <img loading='lazy' src="/images/fleetVehicles.jfif" alt="Fleet of vehicles as background" />
         </figure>
       </div>
     </>
